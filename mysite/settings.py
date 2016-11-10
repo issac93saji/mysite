3ea9ac9b3809463587043cb9ed853bfb52f3473c
Nivin Paul <nivin@sayonetech.com>
2016-11-10 19:14:46 +0530

    more

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,6 +1,7 @@
 
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -116,6 +117,11 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+#for heroku
+DATABASES['default']=dj_database_url.config()
+
 
 
 # Password validation
