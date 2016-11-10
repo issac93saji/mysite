d590577c2d9ec9bfa9d08a128aaf2e5208ba6e09
Nivin Paul <nivin@sayonetech.com>
2016-11-10 18:49:56 +0530

    more

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,10 +4,10 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-app_name= 'events'
+
 
 urlpatterns = [
-    url(r'^events/',include('events.urls')),
+    url(r'^',include('events.urls')),
     url(r'^admin/', admin.site.urls),
 	url(r'^accounts/', include('allauth.urls')),
 ]
